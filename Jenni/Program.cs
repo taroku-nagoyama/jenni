7e7b3bb05900da4d794d91e657cdbe14b0b09b10
taroku-nagoyama <taroku.nagoyama@gmail.com>
2019-02-04 03:10:36 -0500

    sample

--- Jenni/Program.cs
+++ Jenni/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,18 @@ namespace Jenni
     {
         static void Main(string[] args)
         {
+            // convention: args[0], template @ ../args[0]-template
+
+            if (args.Length < 1)
+                throw new Exception("need target dir, then convention is: args[0], template @ ../args[0]-template");
+
+            var dirTarget = Path.Combine(Directory.GetCurrentDirectory(), args[0]);
+            var dirTemplate = Path.Combine(Directory.GetCurrentDirectory(), $"{args[0]}-template");
+
+            Debug.WriteLine($"target folder {dirTarget}");
+            Debug.WriteLine($"template folder {dirTemplate}");
+
+            var filesTemplate = Directory.GetFiles(dirTemplate, "*", SearchOption.AllDirectories);
         }
     }
 }
