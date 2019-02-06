9648d8adcd1d07a80da1bb6a3d259ef10e3b5538
taroku-nagoyama <taroku.nagoyama@gmail.com>
2019-02-06 05:38:06 -0500

    admi

--- Jenni/Program.cs
+++ Jenni/Program.cs
@@ -10,6 +10,8 @@ namespace Jenni
 {
     class Program
     {
+        private static Random rara = new Random();
+
         static void Main(string[] args)
         {
             // convention: args[0], template @ ../args[0]-template
@@ -25,15 +27,74 @@ namespace Jenni
 
             var filesTemplate = Directory.GetFiles(dirTemplate, "*", SearchOption.AllDirectories);
 
-            var index = 0;
-            foreach (var page in filesTemplate.Paginazu(20))
+            const int nSteps = 2;
+            const int daysSpan = 600;
+            var dates = MakeDates(TimeSpan.FromDays(daysSpan), nSteps);
+            foreach (var date in dates)
             {
-                Debug.WriteLine($"======= page {index} ========");
-                foreach (var filename in page)
-                {
-                    Debug.WriteLine($"  {filename}");
-                }
+                Debug.WriteLine(">>> " + date);
+                AdminRun($@"
+date {date.Date.ToString("yyyy-MM-dd")}
+time {date.TimeOfDay.ToString("hh\\:mm\\:ss")}");
             }
+            AdminRun($@"
+start ms-settings:dateandtime");
+
+            AdminCleanup();
+
+            //var index = 0;
+            //foreach (var page in filesTemplate.Paginazu(85))
+            //{
+            //    Debug.WriteLine($"======= page {index} ========");
+            //    foreach (var filename in page)
+            //    {
+            //        Debug.WriteLine($"  {filename}");
+            //    }
+            //    index++;
+            //}
         }
+
+        private static DateTime[] MakeDates(TimeSpan timespan, int n)
+        {
+            var nRandKeypoints = n / 2;
+            var nRandKeypointsSample = n / 4;
+            var list = new List<DateTime>();
+            for (int i = 0; i < nRandKeypoints; i++)
+                list.Add(DateTime.Now.Add(-TimeSpan.FromMilliseconds(timespan.TotalMilliseconds * rara.NextDouble())));
+            for (int i = nRandKeypoints; i < n; i++)
+                list.Add(list[rara.Next(nRandKeypointsSample)].Add(TimeSpan.FromSeconds(rara.Next(3000, 48*3600))));
+            return list.OrderBy(x => x).ToArray();
+        }
+
+        private static void AdminRun(string batCode)
+        {
+            var tmpBat = BatFilename();
+            File.WriteAllText(tmpBat, batCode);
+            Process.Start(new ProcessStartInfo(tmpBat)
+            {
+                CreateNoWindow = true,
+                ErrorDialog = false,
+                UseShellExecute = false,
+                RedirectStandardError = true,
+                Verb = "runas"
+            }).WaitForExit();
+        }
+
+        private static void AdminCleanup()
+        {
+            File.Delete(BatFilename());
+        }
+
+        private static string BatFilename()
+        {
+            return Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                "ubliabl\\camel-with-67-humps.bat"
+            );
+        }
+
+        
+
+
     }
 }
