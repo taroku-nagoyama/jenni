91fcb5bc07d057162a1a30a079b33ccf7baf874c
taroku-nagoyama <taroku.nagoyama@gmail.com>
2019-02-07 00:28:58 -0500

    message

--- Jenni/Program.cs
+++ Jenni/Program.cs
@@ -17,41 +17,79 @@ namespace Jenni
             // convention: args[0], template @ ../args[0]-template
 
             if (args.Length < 1)
-                throw new Exception("need target dir, then convention is: args[0], template @ ../args[0]-template");
+                throw new Exception("need template dir as first argument");
 
-            var dirTarget = Path.Combine(Directory.GetCurrentDirectory(), args[0]);
-            var dirTemplate = Path.Combine(Directory.GetCurrentDirectory(), $"{args[0]}-template");
-
-            Debug.WriteLine($"target folder {dirTarget}");
-            Debug.WriteLine($"template folder {dirTemplate}");
+            var dirTemplate = Path.Combine(Directory.GetCurrentDirectory(), args[0]);
+            Debug.WriteLine($"template folder: {dirTemplate}");
 
             var filesTemplate = Directory.GetFiles(dirTemplate, "*", SearchOption.AllDirectories);
 
             const int nSteps = 2;
             const int daysSpan = 600;
+            var amts = MakeAmounts(nSteps, Math.Min(22, filesTemplate.Length / 9), filesTemplate.Length);
             var dates = MakeDates(TimeSpan.FromDays(daysSpan), nSteps);
-            foreach (var date in dates)
+            for (int i = 0; i < dates.Length; i++)
             {
+                var date = dates[i];
+                var amt = amts[i];
                 Debug.WriteLine(">>> " + date);
-                AdminRun($@"
-date {date.Date.ToString("yyyy-MM-dd")}
-time {date.TimeOfDay.ToString("hh\\:mm\\:ss")}");
+
+                var iFile = 0;
+                foreach (var filefinal in filesTemplate)
+                {
+                    var destfname = filefinal.Replace(dirTemplate, Directory.GetCurrentDirectory());
+                    var fileContents = File.ReadAllText(filefinal);
+
+                    // TODO: alter
+
+                    if (!Directory.Exists(Path.GetDirectoryName(destfname)))
+                        Directory.CreateDirectory(Path.GetDirectoryName(destfname));
+                    File.WriteAllText(destfname, fileContents);
+                    iFile++;
+                    if (iFile >= amt)
+                        break;
+                }
+
+                var message = rara.NextDouble() < 0.3
+                    ? "wip"
+                    : rara.NextDouble() < 0.3
+                    ? "fix..."
+                    : rara.NextDouble() < 0.2
+                    ? "broken!.."
+                    : "...";
+                if (rara.NextDouble() < 0.6)
+                    message = "TN - " + message;
+
+//                AdminRun($@"
+//date {date.Date.ToString("yyyy-MM-dd")}
+//time {date.TimeOfDay.ToString("hh\\:mm\\:ss")}
+//git add .
+//git commit -m ""wippppp - {date}""");
+
+//                AdminRun($@"
+//git add .
+//git commit -a -m ""{message}""");
+
+                Debug.WriteLine($@"
+git add .
+git commit -a -m ""{message}""");
             }
-            AdminRun($@"
-start ms-settings:dateandtime");
+
+            AdminRun($@"start ms-settings:dateandtime");
+
+            foreach (var filefinal in filesTemplate)
+            {
+                var destfname = filefinal.Replace(dirTemplate, Directory.GetCurrentDirectory());
+                if (!Directory.Exists(Path.GetDirectoryName(destfname)))
+                    Directory.CreateDirectory(Path.GetDirectoryName(destfname));
+                File.Copy(filefinal, destfname, true);
+            }
+
+//            AdminRun($@"
+//git add .
+//git commit -m ""...""");
 
             AdminCleanup();
-
-            //var index = 0;
-            //foreach (var page in filesTemplate.Paginazu(85))
-            //{
-            //    Debug.WriteLine($"======= page {index} ========");
-            //    foreach (var filename in page)
-            //    {
-            //        Debug.WriteLine($"  {filename}");
-            //    }
-            //    index++;
-            //}
         }
 
         private static DateTime[] MakeDates(TimeSpan timespan, int n)
@@ -63,6 +101,14 @@ start ms-settings:dateandtime");
                 list.Add(DateTime.Now.Add(-TimeSpan.FromMilliseconds(timespan.TotalMilliseconds * rara.NextDouble())));
             for (int i = nRandKeypoints; i < n; i++)
                 list.Add(list[rara.Next(nRandKeypointsSample)].Add(TimeSpan.FromSeconds(rara.Next(3000, 48*3600))));
+            return list.OrderBy(x => x).ToArray();
+        }
+
+        private static int[] MakeAmounts(int n, int min, int max)
+        {
+            var list = new List<int>();
+            for (int i = 0; i < n; i++)
+                list.Add(rara.Next(min, max));
             return list.OrderBy(x => x).ToArray();
         }
 
